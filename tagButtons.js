cd158aa571ebd2445302dc757e3f51c40dfe6d37
Ryan Koning <rkoning8@gmail.com>
2017-12-04 23:02:31 -0700

    Created Tag Cloud

---
+++ tagButtons.js
@@ -0,0 +1,61 @@
+var testTags = ["you", "have", "entered", "the", "tag", "zone", "things", "not", "even", "a", "real", "shark"];
+var tagDiv = document.getElementById("tagZone");
+var selectedList = document.getElementById("selectedTags");
+var selected = [];
+
+ConstructButtons(testTags);
+
+function SetState(b) {
+	if(!selected.includes(b))  {
+		selected.push(b);
+		b.className = "down";
+		AddToList(b.innerHTML);
+	} else {
+		const index = selected.indexOf(b);
+		selected.splice(index, 1);
+		b.className = "up";
+		RemoveFromList(b.innerHTML);
+	}
+	
+	var radnum = b.value.replace(/[^1-9]/gi,"");
+}
+
+function AddToList(tagName) {
+	var listItem = document.createElement("li");
+	
+	listItem.innerHTML = tagName;
+	listItem.id = "_" + tagName;
+	listItem.style.color = GetRandomColor();
+	selectedList.appendChild(listItem);
+}
+
+function RemoveFromList(tagName) {
+	var listItem = document.getElementById("_" + tagName);
+	
+	listItem.parentNode.removeChild(listItem);
+}
+
+function GetRandomColor() {
+	var r = (Math.floor(Math.random() * 256));
+	var g = (Math.floor(Math.random() * 256));
+	var b = (Math.floor(Math.random() * 256));
+	
+	return 'rgb(' + r + ',' + g + ',' + b + ')';
+	
+}
+
+function ConstructButtons(tagList) {
+	
+	for (var i = 0; i < tagList.length; i++) {
+		
+		var btn = document.createElement("button");
+		
+		btn.id = "button" + i;
+		btn.setAttribute("class", "up");
+		btn.innerHTML = tagList[i];
+		
+		btn.onclick = function() {SetState(this);};
+		tagDiv.appendChild(btn);
+	}
+	
+}
