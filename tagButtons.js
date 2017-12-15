c0bba3ab7bb913047e294e262dec312b334fa6ad
Unknown <mathewgutierrez00@gmail.com>
2017-12-15 15:01:01 -0700

    Got it to the point where it will only redraw selected tags.
    Known Bugs: Old circles still show up. Redraw map onto SVG?

--- tagButtons.js
+++ tagButtons.js
@@ -2,7 +2,7 @@
 // var tagDiv = document.getElementById("tagZone");
 // var selectedList = document.getElementById("selectedTags");
 // var selected = [];
-
+	var tagArray = [];
 // ConstructButtons(testTags);
 
 function SetState(b) {
@@ -16,13 +16,14 @@ function SetState(b) {
 		b.className = "up";
 		RemoveFromList(b.innerHTML);
 	}
-	
+
 	var radnum = b.value.replace(/[^1-9]/gi,"");
 }
 
 function AddToList(tagName) {
 	var listItem = document.createElement("li");
-	
+	tagArray.push(tagName);
+
 	listItem.innerHTML = tagName;
 	listItem.id = "_" + tagName;
 	listItem.style.color = GetRandomColor();
@@ -31,7 +32,10 @@ function AddToList(tagName) {
 
 function RemoveFromList(tagName) {
 	var listItem = document.getElementById("_" + tagName);
-	
+	var index = tagArray.indexOf(tagName);
+
+	tagArray.splice(index, 1);
+
 	listItem.parentNode.removeChild(listItem);
 }
 
@@ -39,23 +43,30 @@ function GetRandomColor() {
 	var r = (Math.floor(Math.random() * 256));
 	var g = (Math.floor(Math.random() * 256));
 	var b = (Math.floor(Math.random() * 256));
-	
+
 	return 'rgb(' + r + ',' + g + ',' + b + ')';
-	
+
 }
 
 function ConstructButtons(tagList) {
-	
+
 	for (var i = 0; i < tagList.length; i++) {
-		
+
 		var btn = document.createElement("button");
-		
+
 		btn.id = "button" + i;
 		btn.setAttribute("class", "up");
 		btn.innerHTML = tagList[i];
-		
-		btn.onclick = function() {SetState(this);};
+
+		btn.onclick = function() {
+
+			updateView();
+			SetState(this);
+		};
+
 		tagDiv.appendChild(btn);
 	}
-	
+}
+function getTagArray() {
+	return tagArray;
 }
